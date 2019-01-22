7227898a1b1daec7dc548f475a687531a4b632bb
Convex <convexprime@gmail.com>
2019-01-22 04:21:45 -0500

    user account and profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -36,6 +37,7 @@ class UpdateAccount(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
@@ -45,7 +47,7 @@ class UpdateAccount(FlaskForm):
                 raise ValidationError('That username is taken.  Please choose a different one.')
 
     def validate_email(self, email):
-        if username.data != current_user.username:
+        if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken.  Please choose a different one.')
